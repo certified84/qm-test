9d418f52459c3d0f7691e4586e3ab0780aa3b35e
Samson Achiaga <sirhumble84@gmail.com>
2024-09-05 22:01:32 +0100

    Update home screen

--- app/index.tsx
+++ app/index.tsx
@@ -8,14 +8,9 @@ import {
   TouchableOpacity,
   ScrollView,
 } from "react-native";
-import {
-  AddCircle20Regular,
-  Alert24Regular,
-  EraserSmall20Regular,
-  QuestionCircle24Filled,
-} from "@fluentui/react-native-icons";
 import { HelloWave } from "@/components/HelloWave";
 import ShareBig from "@/assets/icons/share-big.icons";
+import RemixIcon from "react-native-remix-icon";
 
 export default function Index() {
   try {
@@ -47,31 +42,31 @@ export default function Index() {
               activeOpacity={0.5}
               className="flex-row py-2 px-3 items-center border-[1px] border-[#E4F1FA] mr-2 rounded-3xl"
             >
-              <EraserSmall20Regular color="#E4F1FA" />
+              <RemixIcon name="eraser-line" size={18} color="#E4F1FA" />
               <Text className="text-[#E4F1FA] ml-1">0</Text>
             </TouchableOpacity>
             <TouchableOpacity
               activeOpacity={0.5}
               className="flex-row p-2 px-3 bg-[#E4F1FA] rounded-3xl items-center"
             >
-              <Text className="text-[#17478B]">₦5,000.00</Text>
-              <AddCircle20Regular color="#17478B" />
+              <Text className="text-[#17478B] text-sm font-medium">₦5,000.00</Text>
+              <RemixIcon name="add-circle-line" size={18} color="#17478B" />
             </TouchableOpacity>
 
             <TouchableOpacity className="ml-3" activeOpacity={0.5}>
-              <Alert24Regular color="#E4F1FA" />
+              <RemixIcon name="notification-3-line" color="#E4F1FA" />
             </TouchableOpacity>
           </View>
         </View>
 
         <View className="mt-4 flex-row">
-          <Text className="text-white">Hello John</Text>
+          <Text className="text-white text-2xl font-semibold">Hello John</Text>
           <HelloWave />
         </View>
-        <Text className="text-white">Let's play and earn</Text>
+        <Text className="text-white mt-[4px] text-base">Let's play and earn</Text>
       </ImageBackground>
 
-      <ScrollView className="absolute w-full top-[230px] px-4">
+      <ScrollView className="absolute w-full top-[230px] px-4 pb-28">
         <View className="shadow-md">
           <ImageBackground
             className=" bg-white rounded-3xl overflow-hidden"
@@ -82,25 +77,25 @@ export default function Index() {
               hitSlop={5}
               className="self-end m-5"
             >
-              <QuestionCircle24Filled color="#51A2E0" />
+              <RemixIcon name="question-fill" color="#51A2E0" />
             </TouchableOpacity>
 
             <View className="items-center justify-center my-2">
-              <Text className="text-[#17478B]">Game Prize</Text>
-              <Text className="text-[#17478B] my-2">1,000,000</Text>
-              <Text>Next Game: Tomorrow, 8PM</Text>
+              <Text className="text-[#17478B] text-2xl font-bold">Game Prize</Text>
+              <Text className="text-[#17478B] text-5xl font-bold my-2">1,000,000</Text>
+              <Text className="font-medium text-base">Next Game: Tomorrow, 8PM</Text>
             </View>
 
             <View className="bg-[#17478B] flex-row p-4 mt-8 justify-between items-center">
               <TouchableOpacity className="bg-white p-2 rounded-3xl px-3">
                 <Text>Join Game</Text>
               </TouchableOpacity>
-              <Text className="text-white">Entry fee: ₦10.00</Text>
+              <Text className="text-white font-bold">Entry Fee <Text className="font-normal">₦100.00</Text></Text>
             </View>
           </ImageBackground>
         </View>
 
-        <Text className="mt-6">Top Gamers of the day</Text>
+        <Text className="mt-6 font-bold text-lg">Top Gamers of the day</Text>
         <View className="flex-row justify-between mt-5">
           {topGamers.map((item, index) => (
             <TopGamer
@@ -112,18 +107,20 @@ export default function Index() {
           ))}
         </View>
 
-        <View className="bg-[#2364AA] p-6 mt-8 rounded-[20px] gap-2">
-          <ShareBig />
-          <Text className="text-white">Refer and Earn with your friends</Text>
-          <Text className="text-white">
-            Share with your friends and loved ones to come and play
-          </Text>
-          <TouchableOpacity
-            activeOpacity={0.5}
-            className="border border-[#E4F1FA] self-baseline p-3 rounded-[20px] mt-4"
-          >
-            <Text className="text-white">Invite Friends</Text>
-          </TouchableOpacity>
+        <View className="bg-[#2364AA] p-6 pr-0 mt-8 rounded-[20px] flex-row items-center">
+          <View className="flex-1">
+            <Text className="text-white font-bold text-xl">Refer and Earn with your friends</Text>
+            <Text className="text-white py-2">
+              Share with your friends and loved ones to come and play
+            </Text>
+            <TouchableOpacity
+              activeOpacity={0.5}
+              className="border border-[#E4F1FA] self-baseline p-3 rounded-[20px] mt-4"
+            >
+              <Text className="text-white">Invite Friends</Text>
+            </TouchableOpacity>
+          </View>
+          <ShareBig style={{ flex: 1 }} />
         </View>
       </ScrollView>
     </View>
@@ -143,8 +140,8 @@ const TopGamer: React.FC<GamersProps> = (props) => (
       style={{ backgroundColor: props.background }}
       className={`rounded-full w-[65px] h-[65px] flex items-center justify-center`}
     ></View>
-    <Text>{props.name}</Text>
-    <Text>{props.price}</Text>
+    <Text className="font-bold">{props.name}</Text>
+    <Text className="text-[##2364AA]">{props.price}</Text>
   </TouchableOpacity>
 );
 
